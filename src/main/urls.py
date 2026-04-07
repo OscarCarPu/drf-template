2e95d88650af97f993bb4c74612bf88284cca604
OCA <oscar.carballo@nazaries.com>
2026-04-07 12:48:52 +0200

    feat: add shortcut routes for API documentation

--- src/main/urls.py
+++ src/main/urls.py
@@ -9,4 +9,7 @@ urlpatterns = [
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path("api/schema/swagger/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
     path("api/schema/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
+    # Shortcuts
+    path("swagger/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger"),
+    path("redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc-shortcut"),
 ]
